c9e6856ca6e1d978a6c59c2384413d90aeab5cd5
maksimussokol <maksimus@2074.ru>
2022-10-02 16:37:34 +0500

    Ex34 works

---
+++ ex34/Program.cs
@@ -0,0 +1,26 @@
+﻿//zadanie34
+Console.Clear();
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+int count = 0;
+void chetchisla(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+chetchisla(array);
+Console.WriteLine($"chetnyh chisel v massive: {count}");
